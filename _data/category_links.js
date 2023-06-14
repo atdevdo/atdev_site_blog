be589cd973d303875b1b15c007e493e4ce5a3e20
AdryAtDev <adry.mateo@atdev.do>
2023-06-14 16:51:40 -0400

    Random quote - added

---
+++ _data/category_links.js
@@ -0,0 +1,22 @@
+/** @format */
+
+module.exports = {
+  links: [
+    {
+      label: 'Community',
+      link: '/categories/community/1',
+    },
+    {
+      label: 'Growth',
+      link: '/categories/growth/1',
+    },
+    {
+      label: 'Remote Work',
+      link: '/categories/remote-work/1',
+    },
+    {
+      label: 'Technology',
+      link: '/categories/technology/1',
+    },
+  ],
+}
