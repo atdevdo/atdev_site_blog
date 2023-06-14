0299f620e1c5dd850e19603decd709a5a14ea513
AdryAtDev <132488153+AdryAtDev@users.noreply.github.com>
2023-06-14 15:18:59 -0400

    Update header.js

--- _data/header.js
+++ _data/header.js
@@ -3,11 +3,26 @@
 module.exports = {
   logo: '/assets/icons/logo.png',
   links: [
-    { label: 'Home', link: 'https://atdev.do/' },
-    { label: 'Services', link: 'https://atdev.do/pages/service/' },
-    { label: 'Careers', link: 'https://atdev.do/pages/careersus/' },
-    { label: 'About', link: 'https://atdev.do/pages/about/' },
-    { label: 'Contact', link: 'https://atdev.do/pages/contact/' },
+    {
+      label: 'Home',
+      link: 'https://tubular-pastelito-306d7f.netlify.app/pages/en/',
+    },
+    {
+      label: 'Services',
+      link: 'https://tubular-pastelito-306d7f.netlify.app/pages/en/service/',
+    },
+    {
+      label: 'Careers',
+      link: 'https://tubular-pastelito-306d7f.netlify.app/pages/en/careersus/',
+    },
+    {
+      label: 'About',
+      link: 'https://tubular-pastelito-306d7f.netlify.app/pages/en/about/',
+    },
+    {
+      label: 'Contact',
+      link: 'https://tubular-pastelito-306d7f.netlify.app/pages/en/contact/',
+    },
     { label: 'Blog', link: '/' },
   ],
 }
