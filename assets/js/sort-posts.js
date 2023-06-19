e5b18731f5105e202f28c88b1f328d105a78a462
AdryAtDev <adry.mateo@atdev.do>
2023-06-19 17:00:11 -0400

    Updates

--- assets/js/sort-posts.js
+++ assets/js/sort-posts.js
@@ -30,18 +30,37 @@ sort_by_title_btn.addEventListener('click', () => {
 	title_counter++
 	if (title_counter % 2 == 0) {
 		sorted_title_box.style.display = 'block'
-		btn_sort_title_img.src = '/assets/icons/sort-az-flipped.png'
 		sort_by_title_btn.setAttribute('class', 'active')
+		btn_sort_title_img.src = '/assets/icons/sort-az-flipped.png'
+
+		sorted_time_box.style.display = 'none'
+		sorted_category_box.style.display = 'none'
+		sorted_date_box.style.display = 'none'
+		not_sorted_box.style.display = 'none'
+
+		btn_sort_time_img.src = '/assets/icons/sort-time-read.png'
+		btn_sort_category_img.src = '/assets/icons/sort-category.png'
+
+		date_counter = 1
+		time_counter = 1
+		category_counter = 1
+
 		sort_by_time_to_read_btn.removeAttribute('class', 'active')
 		sort_by_category_btn.removeAttribute('class', 'active')
-		not_sorted_box.style.display = 'none'
-	}
-
-	if (title_counter % 2 != 0) {
+		sort_by_date_btn.removeAttribute('class', 'active')
+	} else {
 		sorted_title_box.style.display = 'none'
+		sorted_time_box.style.display = 'none'
+		sorted_category_box.style.display = 'none'
+		sorted_date_box.style.display = 'none'
+
 		not_sorted_box.style.display = 'block'
-		sort_by_title_btn.removeAttribute('class', 'active')
 		btn_sort_title_img.src = '/assets/icons/sort-az.png'
+
+		sort_by_title_btn.removeAttribute('class', 'active')
+		sort_by_time_to_read_btn.removeAttribute('class', 'active')
+		sort_by_category_btn.removeAttribute('class', 'active')
+		sort_by_date_btn.removeAttribute('class', 'active')
 	}
 })
 
@@ -49,20 +68,37 @@ sort_by_time_to_read_btn.addEventListener('click', () => {
 	time_counter++
 	if (time_counter % 2 == 0) {
 		sorted_time_box.style.display = 'block'
-		sorted_title_box.style.display = 'none'
 		sort_by_time_to_read_btn.setAttribute('class', 'active')
+		btn_sort_time_img.src = '/assets/icons/sort-time-read-flipped.png'
+
+		sorted_title_box.style.display = 'none'
+		sorted_category_box.style.display = 'none'
+		sorted_date_box.style.display = 'none'
+		not_sorted_box.style.display = 'none'
+
+		btn_sort_title_img.src = '/assets/icons/sort-az.png'
+		btn_sort_category_img.src = '/assets/icons/sort-category.png'
+
+		title_counter = 1
+		date_counter = 1
+		category_counter = 1
+
 		sort_by_title_btn.removeAttribute('class', 'active')
 		sort_by_category_btn.removeAttribute('class', 'active')
-		not_sorted_box.style.display = 'none'
-		btn_sort_time_img.src = '/assets/icons/sort-time-read-flipped.png'
-	}
-
-	if (time_counter % 2 != 0) {
+		sort_by_date_btn.removeAttribute('class', 'active')
+	} else {
 		sorted_time_box.style.display = 'none'
 		sorted_title_box.style.display = 'none'
+		sorted_category_box.style.display = 'none'
+		sorted_date_box.style.display = 'none'
+
 		not_sorted_box.style.display = 'block'
-		sort_by_time_to_read_btn.removeAttribute('class', 'active')
 		btn_sort_time_img.src = '/assets/icons/sort-time-read.png'
+
+		sort_by_time_to_read_btn.removeAttribute('class', 'active')
+		sort_by_title_btn.removeAttribute('class', 'active')
+		sort_by_category_btn.removeAttribute('class', 'active')
+		sort_by_date_btn.removeAttribute('class', 'active')
 	}
 })
 
@@ -70,22 +106,37 @@ sort_by_category_btn.addEventListener('click', () => {
 	category_counter++
 	if (category_counter % 2 == 0) {
 		sorted_category_box.style.display = 'block'
-		sorted_time_box.style.display = 'none'
-		sorted_title_box.style.display = 'none'
-		not_sorted_box.style.display = 'none'
 		sort_by_category_btn.setAttribute('class', 'active')
+		btn_sort_category_img.src = '/assets/icons/sort-category-flipped.png'
+
+		sorted_title_box.style.display = 'none'
+		sorted_time_box.style.display = 'none'
+		sorted_date_box.style.display = 'none'
+		not_sorted_box.style.display = 'none'
+
+		btn_sort_time_img.src = '/assets/icons/sort-time-read.png'
+		btn_sort_title_img.src = '/assets/icons/sort-az.png'
+
+		title_counter = 1
+		time_counter = 1
+		date_counter = 1
+
 		sort_by_title_btn.removeAttribute('class', 'active')
 		sort_by_time_to_read_btn.removeAttribute('class', 'active')
-		btn_sort_category_img.src = '/assets/icons/sort-category-flipped.png'
-	}
-
-	if (category_counter % 2 != 0) {
+		sort_by_date_btn.removeAttribute('class', 'active')
+	} else {
 		sorted_category_box.style.display = 'none'
-		sorted_time_box.style.display = 'none'
 		sorted_title_box.style.display = 'none'
+		sorted_time_box.style.display = 'none'
+		sorted_date_box.style.display = 'none'
+
 		not_sorted_box.style.display = 'block'
-		sort_by_category_btn.removeAttribute('class', 'active')
 		btn_sort_category_img.src = '/assets/icons/sort-category.png'
+
+		sort_by_category_btn.removeAttribute('class', 'active')
+		sort_by_title_btn.removeAttribute('class', 'active')
+		sort_by_time_to_read_btn.removeAttribute('class', 'active')
+		sort_by_date_btn.removeAttribute('class', 'active')
 	}
 })
 
@@ -93,23 +144,36 @@ sort_by_date_btn.addEventListener('click', () => {
 	date_counter++
 	if (date_counter % 2 == 0) {
 		sorted_date_box.style.display = 'block'
-		sorted_category_box.style.display = 'none'
-		sorted_time_box.style.display = 'none'
-		sorted_title_box.style.display = 'none'
-		not_sorted_box.style.display = 'none'
 		sort_by_date_btn.setAttribute('class', 'active')
-		sort_by_category_btn.removeAttribute('class', 'active')
+
+		sorted_title_box.style.display = 'none'
+		sorted_time_box.style.display = 'none'
+		sorted_category_box.style.display = 'none'
+		not_sorted_box.style.display = 'none'
+
+		btn_sort_time_img.src = '/assets/icons/sort-time-read.png'
+		btn_sort_title_img.src = '/assets/icons/sort-az.png'
+		btn_sort_category_img.src = '/assets/icons/sort-category.png'
+
+		title_counter = 1
+		time_counter = 1
+		category_counter = 1
+
 		sort_by_title_btn.removeAttribute('class', 'active')
 		sort_by_time_to_read_btn.removeAttribute('class', 'active')
-	}
-
-	if (date_counter % 2 != 0) {
+		sort_by_category_btn.removeAttribute('class', 'active')
+	} else {
 		sorted_date_box.style.display = 'none'
-		sorted_category_box.style.display = 'none'
-		sorted_time_box.style.display = 'none'
 		sorted_title_box.style.display = 'none'
+		sorted_time_box.style.display = 'none'
+		sorted_category_box.style.display = 'none'
+
 		not_sorted_box.style.display = 'block'
+
 		sort_by_date_btn.removeAttribute('class', 'active')
+		sort_by_title_btn.removeAttribute('class', 'active')
+		sort_by_time_to_read_btn.removeAttribute('class', 'active')
+		sort_by_category_btn.removeAttribute('class', 'active')
 	}
 })
 // END DISPLAY SORTED POSTS
