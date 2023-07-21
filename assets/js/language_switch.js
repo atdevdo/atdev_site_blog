b82e20d349391280bfdb1cbf4137780170bee891
AdryAtDev <adry.mateo@atdev.do>
2023-07-21 13:00:08 -0400

    language switch

--- assets/js/language_switch.js
+++ assets/js/language_switch.js
@@ -8,21 +8,21 @@ let currentPage = 'pages'
 
 // CONDITIONALS
 if (!window.location.href.includes('pages')) {
-  currentPage = 'services'
+	currentPage = 'services'
 }
 
 // CONSTANTS
 const currentUrl = window.location.href
 const slicedUrl = currentUrl.slice(
-  currentUrl.indexOf(`/${currentPage}/${html.lang}/`)
+	currentUrl.indexOf(`/${currentPage}/${html.lang}/`)
 )
 const modifiedUrl = slicedUrl
 const newUrl = modifiedUrl.replace(`/${currentPage}/${html.lang}/`, '')
 
 // lOOPS
 for (let index = 0; index < btn_language.length; index++) {
-  btn_language[index].addEventListener('click', (e) => {
-    // console.log(`/${currentPage}/${e.target.lang}/${newUrl}`)
-    window.location.href = `/${currentPage}/${e.target.lang}/${newUrl}`
-  })
+	btn_language[index].addEventListener('click', (e) => {
+		// console.log(`/${currentPage}/${e.target.lang}/${newUrl}`)
+		window.location.href = `/${currentPage}/${e.target.lang}/${newUrl}`
+	})
 }
