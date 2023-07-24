0ea6c1bd6923e7b3fce979992d72b02447e73470
AdryAtDev <adry.mateo@atdev.do>
2023-07-24 14:15:06 -0400

    language update

--- _11ty/filters/nunjucks-filters.js
+++ _11ty/filters/nunjucks-filters.js
@@ -1,18 +1,18 @@
 /** @format */
 function fixCategory(category) {
-  return category.replace(/-/g, ' ')
+	return category.replace(/-/g, ' ')
 }
 
 function dateNow() {
-  return new Date().toLocaleDateString('en-us', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  })
+	return new Date().toLocaleDateString('en-us', {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+	})
 }
 
 module.exports = {
-  fixCategory,
-  dateNow,
+	fixCategory,
+	dateNow,
 }
---
+++ _data/intro.js
@@ -0,0 +1,16 @@
+module.exports = {
+	base: {
+		text1: {
+			en: 'Blog',
+			es: 'Blog',
+		},
+		text2: {
+			en: 'ATDEV',
+			es: 'ATDEV',
+		},
+		text3: {
+			en: 'We build your ideas, we build your team',
+			es: 'Construimos tus ideas, construimos tu equipo',
+		},
+	},
+}
