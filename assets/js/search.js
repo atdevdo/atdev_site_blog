e09a600011adefb987572dbd52cd2cbe180da1cf
AdryAtDev <adry.mateo@atdev.do>
2023-08-01 10:13:33 -0400

    update

--- assets/js/search.js
+++ assets/js/search.js
@@ -35,7 +35,7 @@
 		}
 	}
 
-	fetch('/search-index-en.json').then((response) =>
+	fetch('/search-index.json').then((response) =>
 		response.json().then((rawIndex) => {
 			window.searchIndex = elasticlunr.Index.load(rawIndex)
 			document
@@ -44,49 +44,96 @@
 		})
 	)
 })(window, document)
-;(function (window, document) {
-	'use strict'
 
-	const searchEs = (e) => {
-		const resultsEs = window.searchIndex.search(e.target.value, {
-			bool: 'OR',
-			expand: true,
-		})
+// ;(function (window, document) {
+// 	'use strict'
 
-		const resElEs = document.getElementById('searchResultsEs')
-		const noResultsElEs = document.getElementById('noResultsFoundEs')
+// 	const search = (e) => {
+// 		const results = window.searchIndex.search(e.target.value, {
+// 			bool: 'OR',
+// 			expand: true,
+// 		})
 
-		resElEs.innerHTML = ''
-		if (resultsEs) {
-			noResultsElEs.style.display = 'none'
-			resultsEs.map((r) => {
-				const { id, title, description } = r.doc
-				const el = document.createElement('li')
-				resElEs.appendChild(el)
+// 		const resEl = document.getElementById('searchResults')
+// 		const noResultsEl = document.getElementById('noResultsFound')
 
-				const h3 = document.createElement('h3')
-				el.appendChild(h3)
+// 		resEl.innerHTML = ''
+// 		if (results) {
+// 			noResultsEl.style.display = 'none'
+// 			results.map((r) => {
+// 				const { id, title, description } = r.doc
+// 				const el = document.createElement('li')
+// 				resEl.appendChild(el)
 
-				const a = document.createElement('a')
-				a.setAttribute('href', id + 'es')
-				a.textContent = title
-				h3.appendChild(a)
+// 				const h3 = document.createElement('h3')
+// 				el.appendChild(h3)
 
-				const p = document.createElement('p')
-				p.textContent = description
-				el.appendChild(p)
-			})
-		} else {
-			noResultsElEs.style.display = 'block'
-		}
-	}
+// 				const a = document.createElement('a')
+// 				a.setAttribute('href', id)
+// 				a.textContent = title
+// 				h3.appendChild(a)
 
-	fetch('/search-index-es.json').then((response) =>
-		response.json().then((rawIndex) => {
-			window.searchIndex = elasticlunr.Index.load(rawIndex)
-			document
-				.getElementById('searchFieldEs')
-				.addEventListener('input', searchEs)
-		})
-	)
-})(window, document)
+// 				const p = document.createElement('p')
+// 				p.textContent = description
+// 				el.appendChild(p)
+// 			})
+// 		} else {
+// 			noResultsEl.style.display = 'block'
+// 		}
+// 	}
+
+// 	fetch('/search-index-en.json').then((response) =>
+// 		response.json().then((rawIndex) => {
+// 			window.searchIndex = elasticlunr.Index.load(rawIndex)
+// 			document
+// 				.getElementById('searchField')
+// 				.addEventListener('input', search)
+// 		})
+// 	)
+// })(window, document)
+// ;(function (window, document) {
+// 	'use strict'
+
+// 	const searchEs = (e) => {
+// 		const resultsEs = window.searchIndex.search(e.target.value, {
+// 			bool: 'OR',
+// 			expand: true,
+// 		})
+
+// 		const resElEs = document.getElementById('searchResultsEs')
+// 		const noResultsElEs = document.getElementById('noResultsFoundEs')
+
+// 		resElEs.innerHTML = ''
+// 		if (resultsEs) {
+// 			noResultsElEs.style.display = 'none'
+// 			resultsEs.map((r) => {
+// 				const { id, title, description } = r.doc
+// 				const el = document.createElement('li')
+// 				resElEs.appendChild(el)
+
+// 				const h3 = document.createElement('h3')
+// 				el.appendChild(h3)
+
+// 				const a = document.createElement('a')
+// 				a.setAttribute('href', id + 'es')
+// 				a.textContent = title
+// 				h3.appendChild(a)
+
+// 				const p = document.createElement('p')
+// 				p.textContent = description
+// 				el.appendChild(p)
+// 			})
+// 		} else {
+// 			noResultsElEs.style.display = 'block'
+// 		}
+// 	}
+
+// 	fetch('/search-index-es.json').then((response) =>
+// 		response.json().then((rawIndex) => {
+// 			window.searchIndex = elasticlunr.Index.load(rawIndex)
+// 			document
+// 				.getElementById('searchFieldEs')
+// 				.addEventListener('input', searchEs)
+// 		})
+// 	)
+// })(window, document)
